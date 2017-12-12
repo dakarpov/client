b8fb210222ae4152152779224beb008b64f075dc
Dmitriy Karpov <d_karpov@wargaming.net>
2017-12-12 21:02:36 +0300

    game class

--- game.py
+++ game.py
@@ -5,20 +5,33 @@ from models.map import Map
 
 import time
 
-loop = asyncio.get_event_loop()
-cl = client.Client(loop)
 
 # 'wgforge-srv.wargaming.net', 443
 # '127.0.0.1', 2000
-loop.run_until_complete(cl.connect('wgforge-srv.wargaming.net', 443))
-_, msg = loop.run_until_complete(cl.login('test-client14'))
-player = Player(msg)
-print(str(player.trains))
-print(str(msg))
-_, msg = loop.run_until_complete(cl.getMap(0))
-map = Map(msg)
-_, msg = loop.run_until_complete(cl.getMap(10))
-map.add_layer_10(msg)
-print(msg)
-loop.run_until_complete(cl.logout())
-print("logout")
+
+class Game(object):
+    def __init__(self, playername):
+        """
+        Initialization of the game. Initializes connection, player and map
+        :param playername: string
+        """
+        self.loop = asyncio.get_event_loop()
+        self.cl = client.Client(self.loop)
+        self.loop.run_until_complete(self.cl.connect('wgforge-srv.wargaming.net', 443))
+        _, msg = self.loop.run_until_complete(self.cl.login(playername))
+        self.player = Player(msg)
+        # print(str(player.trains))
+        # print(str(msg))
+        _, msg = self.loop.run_until_complete(self.cl.getMap(0))
+        self.map = Map(msg)
+        _, msg = self.loop.run_until_complete(self.cl.getMap(10))
+        self.map.add_layer_10(msg)
+        # print(msg)
+
+    def __del__(self):
+        self.loop.run_until_complete(self.cl.logout())
+        print("logout")
+
+
+game = Game('test-client18')
+del game
